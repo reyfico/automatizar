0b5e5e5eac70f3e633b339b35e59613ca0187769
federico de simone <federicodesimone30@gmail.com>
2022-09-06 04:41:37 -0300

    comprime imagenes

--- comprimir.py
+++ comprimir.py
@@ -8,6 +8,9 @@ downloadsfolder = "/home/fx/Pictures/PRTG/"
 if __name__ == "__main__":
     for filename in os.listdir(downloadsfolder):
         name, extension = os.path.splitext(downloadsfolder + filename)
-        print (filename)
+        #print (filename)
         #print (name)
-        print (extension)
+        #print (extension)
+        if extension in [".jpg", ".png", ".jpeg"]:
+            picture = Image.open(downloadsfolder + filename)
+            picture.save(downloadsfolder + "comprimida_"+filename, optimize=True, quality=60)
